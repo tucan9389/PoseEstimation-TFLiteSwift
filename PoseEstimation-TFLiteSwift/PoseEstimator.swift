c030d1ef6eb0d15ce79553de8725f863edac0ad3
Doyoung Gwak <tucan.dev@gmail.com>
2020-03-14 22:02:53 +0900

    Mvoe some structures to inner enum

--- PoseEstimation-TFLiteSwift/PoseEstimator.swift
+++ PoseEstimation-TFLiteSwift/PoseEstimator.swift
@@ -8,34 +8,33 @@
 
 import CoreVideo
 
-struct PoseEstimationKeypoint {
-    let position: CGPoint
-    let score: CGFloat
-}
-
-struct PoseEstimationHeatmaps {
-    // <#TODO#>
-    
-    init(tfliteResult: TFLiteResult) {
-        // <#TODO#>
-    }
-    
-    var keypointsAndScore: [(position: CGPoint, score: CGFloat)] {
-        // <#TODO#>
-        return []
-    }
-}
-
 enum PoseEstimationResult {
-    case success(keypoints: [PoseEstimationKeypoint], heatmaps: PoseEstimationHeatmaps)
+    case success(keypoints: [Keypoint], heatmaps: Heatmaps)
     case fail
+    
+    struct Keypoint {
+        let position: CGPoint
+        let score: CGFloat
+    }
+    struct Heatmaps {
+        // <#TODO#>
+        
+        init(tfliteResult: TFLiteResult) {
+            // <#TODO#>
+        }
+        
+        var keypointsAndScore: [(position: CGPoint, score: CGFloat)] {
+            // <#TODO#>
+            return []
+        }
+    }
 }
 
 /// for TensorFlowLite model
 extension PoseEstimationResult {
     init(tfliteResult: TFLiteResult) {
-        let heatmaps = PoseEstimationHeatmaps(tfliteResult: tfliteResult)
-        let keypoints = heatmaps.keypointsAndScore.map { PoseEstimationKeypoint(position: $0.position, score: $0.score) }
+        let heatmaps = Heatmaps(tfliteResult: tfliteResult)
+        let keypoints = heatmaps.keypointsAndScore.map { Keypoint(position: $0.position, score: $0.score) }
         // <#TODO#>
         self = .success(keypoints: keypoints, heatmaps: heatmaps)
     }
