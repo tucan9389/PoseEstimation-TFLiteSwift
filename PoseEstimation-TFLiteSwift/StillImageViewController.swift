2ae16511402caafa6cd7deaf8d758cbe4907b729
Doyoung Gwak <tucan.dev@gmail.com>
2020-03-20 18:02:39 +0900

    Add StillImageVC and rename VC to LiveImageVC

---
+++ PoseEstimation-TFLiteSwift/StillImageViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StillImageViewController.swift
+//  PoseEstimation-TFLiteSwift
+//
+//  Created by Doyoung Gwak on 2020/03/20.
+//  Copyright © 2020 Doyoung Gwak. All rights reserved.
+//
+
+import UIKit
+
+class StillImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
