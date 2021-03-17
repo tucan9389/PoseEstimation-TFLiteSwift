e33f76f9a91dfe62e04a30e426287abc9f62062c
doyoung-gwak <tucan9389@gmail.com>
2021-03-17 16:00:07 +0900

    Implement HPH 3D pose estimation pre/post processing

--- PoseEstimation-TFLiteSwift/Live3DRenderingViewController.swift
+++ PoseEstimation-TFLiteSwift/Live3DRenderingViewController.swift
@@ -32,7 +32,7 @@ class Live3DRenderingViewController: UIViewController {
     }
     
     // MARK: - ML Property
-    let poseEstimator: PoseEstimator = Baseline3DPoseEstimator()
+    let poseEstimator: PoseEstimator = LiteBaseline3DPoseEstimator()
     var outputHuman: PoseEstimationOutput.Human3D? {
         didSet {
             DispatchQueue.main.async {
